f930209c1c92644fa5de407d301cda81c812154e
sungjun <junjun607@gmail.com>
2022-02-18 21:13:52 +0900

    User database added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const sqlite = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { createBrotliCompress } = require('zlib');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+
+app.use(express.static(path.join(__dirname,'/build')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 let db = new sqlite.Database('./Products.sqlite', (err) => {
     if(err) {
@@ -11,11 +24,54 @@ let db = new sqlite.Database('./Products.sqlite', (err) => {
     }
 })
 
-app.use(express.static(path.join(__dirname,'/build')));
-
 // app.get('/',(req, res) => {
 //     res.sendFile(path.join(__dirname,'build/index.html'));
 // })
+
+app.post('/register',(req,res) => {
+    const newUser = req.body;
+    let Password = null;
+    bcrypt.genSalt(saltRounds, (err,salt) => {
+        if(err){ console.log(err.message);}
+
+        bcrypt.hash(newUser.Password,salt,(err,hash) => {
+            Password = hash;
+            const query = `INSERT INTO Users (ID,Password) VALUES('${newUser.accountId}','${Password}')`;
+            db.run(query,(err) => {
+                if(err) {
+                    return console.log(err.message);
+                }
+
+                console.log(`New user added: ${newUser.accountId}`);
+            })
+            res.json({success:true});
+        })
+    })
+        
+    
+})
+
+app.post('/login', (req,res) => {
+    const accountId = req.body.accountId;
+    const Password = req.body.Password;
+    const query = `SELECT * FROM Users WHERE ID = '${accountId}' `;
+    db.all(query,(err,row) => {
+        console.log(row[0].ID);
+        bcrypt.compare(Password,row[0].Password,(err,isMatch) => {
+            if(err){
+                console.log(err.message);
+                res.json({success: false});
+            }
+            let token = jwt.sign(row[0].ID,'something');
+            const query = `UPDATE Users SET Token = '${token}' WHERE ID = '${row[0].ID}'`;
+            db.run(query,(err) => {
+                res.cookie("x_auth",token).json({loginSuccess: true});
+            })
+            
+        })
+    })
+})
+
 app.get('/images/:imgsrc',(req, res) => {
     res.sendFile(path.join(__dirname,`images/${req.params.imgsrc}`));
 })
