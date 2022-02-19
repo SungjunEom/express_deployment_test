04d57e811e8dbdf92b2c7a25d04b94ffd6938e12
sungjun <junjun607@gmail.com>
2022-02-20 00:26:04 +0900

    메뉴바 수정

--- MainPage/src/App.js
+++ MainPage/src/App.js
@@ -19,20 +19,22 @@ function test2() {
 export default function App() {
   return (
     <div className="App">
-      <div className="head">
-        <h1 className="site-headline">TEST WEBPAGE</h1>
-        <div className="menu">
-          <Link to ="/Home"><BoldTextButton width="50px" height="50px" value="HOME" onClick={test} /></Link>
-          <Link to ="/Products"><BoldTextButton width="70px" height="50px" value="PRODUCTS" onClick={test} /></Link>
-          <Link to ="/ButtonParty"><BoldTextButton width="70px" height="50px" value="BUTTONS TEST" onClick={test} /></Link>
-          <Link to ="/Test"><BoldTextButton width="70px" height="50px" value="LOGIN TEST" /></Link>
-          <Link to ="/ContactUs"><BoldTextButton width="130px" height="50px" value="CONTACT US" onClick={test} /></Link>
+      <div className="head-content">
+        <div className="head">
+          <div id="head-container">
+            <h1 className="site-headline">TEST WEBPAGE</h1>
+            <div className="menu">
+              <Link to ="/Home"><BoldTextButton width="36px" height="30px" value="HOME" onClick={test} /></Link>
+              <Link to ="/Products"><BoldTextButton width="83px" height="30px" value="PRODUCTS" onClick={test} /></Link>
+              <Link to ="/ButtonParty"><BoldTextButton width="70px" height="30px" value="BUTTONS TEST" onClick={test} /></Link>
+              <Link to ="/Test"><BoldTextButton width="40px" height="30px" value="LOGIN TEST" /></Link>
+              <Link to ="/ContactUs"><BoldTextButton width="70px" height="30px" value="CONTACT US" onClick={test} /></Link>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="body-content">
-      <Outlet />
-      </div>
-      <footer>
+        <div className="body-content">
+        <Outlet />
+        <footer>
         <div className="footer-hr"><hr/></div>
         <div className="footer-content">
         <div style={{paddingTop:"6rem"}}>
@@ -45,6 +47,8 @@ export default function App() {
           </div>
         </div>
       </footer>
+        </div>
+      </div>
     </div>
   );
 }
--- MainPage/src/Products.js
+++ MainPage/src/Products.js
@@ -8,18 +8,24 @@ import {Outlet, Link} from "react-router-dom";
 
 export default function Products() {
   return (
-    <div className="products">
-      <div className="products-menu">
-        <h3 style={{fontSize:"2rem"}}>We are selling now:</h3>
-        <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
-        <Link to="/Products/Test2"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pikachu"/></div></Link>
-        <Link to="/Products/IT"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="IT"/></div></Link>
-        <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
-        <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
-        <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
-        <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+    <div className="products-wrapper">
+      <div style={{paddingTop:"50px"}}>
+      <h1>PRODUCTS</h1>
       </div>
-      <Outlet />
+      <div style={{width:"80%", paddingBottom:"50px"}}><hr /></div>
+      <div className="products">
+        <div className="products-menu">
+          <h3 style={{fontSize:"2rem"}}>We are selling now:</h3>
+          <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+          <Link to="/Products/Test2"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pikachu"/></div></Link>
+          <Link to="/Products/IT"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="IT"/></div></Link>
+          <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+          <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+          <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+          <Link to="/Products/Pokemons"><div style={{paddingTop:".1rem",paddingBottom:".1rem"}}><BlackRoundButton width="10rem" height="2rem" value="Pokemons"/></div></Link>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 }
