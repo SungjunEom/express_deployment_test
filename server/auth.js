5ceae3eb53def61fc14c6e9329d91a06f54c1d45
sungjun <junjun607@gmail.com>
2022-02-20 14:39:48 +0900

    auth 미들웨어 수정

--- server/auth.js
+++ server/auth.js
@@ -12,7 +12,7 @@ let auth = (req, res, next) => {
     let token = req.cookies.x_auth;
     let userId = null;
     jwt.verify(token, 'something', (err,decoded) => {
-        const query = `SELECT * FROM Users WHERE ID =  'decoded'`;
+        const query = `SELECT * FROM Users WHERE ID =  '${decoded}' AND Token = '${token}'`;
         db.all(query,(err,row) => {
             if(row.length == 0) {
                 return res.json({auth:false});
